fc8b04894da84a00d17279d30dcf8db7753b8780
arrowrowe <arrowrowe@gmail.com>
2016-04-23 22:08:32 +0800

    :hocho: Wrap the Model instead of just Stringify it

--- util/format.py
+++ util/format.py
@@ -1,4 +1,25 @@
 import log
 
-def strModel(model, title='Model'):
-  return log.title(title) + '\n\n' + str(model.fit().summary()) + '\n'
+def joinAllStr(array, delimeter='\n'):
+  return delimeter.join(map(str, array))
+
+class Model:
+
+  def __init__(self, model, title='Model'):
+    self.model = model
+    self.title = title
+    self._fit = None
+    self._summary = None
+
+  def __str__(self):
+    return log.title(self.title) + '\n\n' + str(self.summary()) + '\n'
+
+  def fit(self):
+    if self._fit is None:
+      self._fit = self.model.fit()
+    return self._fit
+
+  def summary(self):
+    if self._summary is None:
+      self._summary = self.fit().summary()
+    return self._summary
